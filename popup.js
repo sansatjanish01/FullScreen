0c42aeba97721b92ec7f683763085c7b9a1c58df
Meena Yadav <148996539+sansatjanish01@users.noreply.github.com>
2023-11-30 04:43:58 +0000

    modified to get exit as icon is pressed

--- popup.js
+++ popup.js
@@ -1,9 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var fullscreenButton = document.getElementById('fullscreenButton');
-    fullscreenButton.addEventListener('click', function() {
-      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, {toggleFullscreen: true});
-      });
-    });
-  });
-  
+chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+  chrome.tabs.sendMessage(tabs[0].id, { toggleFullscreen: true });
+  window.close(); // Close the popup after triggering fullscreen
+});
